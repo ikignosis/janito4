7c6911962f020783fa4a233e0ca7958b279af2d8
João Pinto <lamego.pinto@gmail.com>
2026-02-14 10:09:20 +0100

    Add system_prompt.py

---
+++ janito4/system_prompt.py
@@ -0,0 +1,5 @@
+SYSTEM_PROMPT = """
+- Before answering to the user, explore the content related to the question
+- Before creating files search the code for the location related to the file purpose
+- Use the namespace functions to deliver the code changes instead of showing the code.
+"""
